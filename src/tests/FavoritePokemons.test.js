a949154a3091b8f9695e2c7a56979ce6b8956a97
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-07-19 18:11:39 -0300

    Conclusão do terceiro requisito - Requisito 03/07

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Conjunto de teste relacionados a rota Favorite Pokemons', () => {
+  test('Verifica se é exibido na tela a mensagem "no favorite pokemons"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const message = screen.getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
