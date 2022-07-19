7a777e70a2562c92756d2c7dfd9ab007ee86e182
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-07-19 17:55:28 -0300

    Conclusão do segundo requisito - Requisito 02/07

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../helper/renderWithRouter';
+
+describe('Conjunto de teste relacionados a rota About', () => {
+  test('Verifica se a página contém informações sobre a pokédex', () => {
+    renderWithRouter(<About />);
+    const firstDescription = screen.getByText(/This application simulates a Pokédex/i);
+    const secondDescription = screen.getByText(/One can filter Pokémons by type/i);
+    expect(firstDescription).toBeInTheDocument();
+    expect(secondDescription).toBeInTheDocument();
+  });
+  test('Verifica se existe uma tag h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', {
+      name: /About pokédex/i,
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+  test('Verifica se a imagem possuí o url determinado', () => {
+    renderWithRouter(<About />);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = screen.getByRole('img', {
+      name: 'Pokédex',
+    });
+    expect(image).toHaveAttribute('src', url);
+  });
+});
